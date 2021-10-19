2cd63917f4aa2b4ef79f1d2dbdeb10f095d102a6
Raphael Vignau <gondawraf@hotmail.fr>
2021-10-19 13:17:43 +0200

    correction

--- src/app/collecte/data/Collecte.ts
+++
@@ -1,8 +0,0 @@
-import {CollecteStatus} from "./collecte.status";
-import {User} from "../../user/data/User";
-
-export interface Collecte {
-    id?: string;
-    status: CollecteStatus;
-    user: User | string;
-}
--- src/app/collecte/data/collecte.status.ts
+++
@@ -1,5 +0,0 @@
-export enum CollecteStatus {
-    ALMOST_FULL = 'almost full',
-    FULL = 'full',
-    COLLECTED = 'collected'
-}
