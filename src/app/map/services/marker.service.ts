cd0466b26bcd742794e6d8bf89c8c433d18d3ba1
Raphael Vignau <gondawraf@hotmail.fr>
2022-01-18 11:43:56 +0100

    Correction markers

--- src/app/map/services/marker.service.ts
+++ src/app/map/services/marker.service.ts
@@ -1,78 +1,76 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
 import * as L from 'leaflet';
-import {PopupService} from './popup.service';
-import {map} from "rxjs/operators";
-import {Observable} from "rxjs";
-import {environment} from "../../../environments/environment";
 import {User} from "../../user/data/User";
+import {LayerGroup} from "leaflet";
 
 @Injectable({
     providedIn: 'root'
 })
 export class MarkerService {
-    private authUrl = environment.api_base_url;
-    acteurs: string = '/assets/data/acteurs_consigne.geojson';
 
     constructor(
-        private http: HttpClient,
-        private popupService: PopupService
     ) {
     }
-/*
-    static scaledRadius(val: number, maxVal: number): number {
-        return 20 * (val / maxVal);
-    }
-*/
 
-    makeCapitalMarkers(map: L.Map, users: User[]): void {
+    makeCapitalMarkers(map: L.Map, users: User[]): { Producteurs: LayerGroup<any>; Distributeurs: LayerGroup<any>; "Points de collectes": LayerGroup<any> } {
 
-        var producersIcon = L.icon({
-            iconUrl: '/assets/img/producers.png',       
-            iconSize:     [35, 35] // size of the icon
+        let producers = L.layerGroup();
+        let resellers = L.layerGroup();
+        let collecte_points = L.layerGroup();
+
+        const producersIcon = L.icon({
+            iconUrl: '/assets/img/producers.png',
+            iconSize: [35, 35] // size of the icon
         });
 
-        var resellersIcon = L.icon({
-            iconUrl: '/assets/img/resellers.png',       
-            iconSize:     [35, 35] // size of the icon
+        const resellersIcon = L.icon({
+            iconUrl: '/assets/img/resellers.png',
+            iconSize: [35, 35] // size of the icon
         });
 
-        var collectePointIcon = L.icon({
-            iconUrl: '/assets/img/collectePoint.png',       
-            iconSize:     [35, 35] // size of the icon
+        const collectePointIcon = L.icon({
+            iconUrl: '/assets/img/collectePoint.png',
+            iconSize: [35, 35] // size of the icon
         });
-        // if :
-        
+
         users.map(user => {
             if (user.lat && user.lon) {
-                const marker = L.marker([Number(user.lat), Number(user.lon)], {icon:producersIcon}).bindPopup('<div> nom : '+user.username +'</div>' + '<div> mail : ' + user.email+'</div>');
-                marker.addTo(map);
-            }
-            else if (user.lat && user.lon && user.producer) {
-                const marker = L.marker([Number(user.lat), Number(user.lon)], {icon:producersIcon}).bindPopup('<div> nom : '+user.username +'</div>' + '<div> mail : ' + user.email+'</div>');
-                marker.addTo(map);
+
+                if (user.producer) {
+                    L.marker(
+                        [Number(user.lat), Number(user.lon)],
+                        {icon: producersIcon}
+                    ).bindPopup(
+                        '<div> nom : ' + user.username + '</div>' + '<div> mail : ' + user.email + '</div>'
+                    ).addTo(producers);
+                }
+
+                if (user.reseller) {
+                    L.marker(
+                        [Number(user.lat), Number(user.lon)],
+                        {icon: resellersIcon}
+                    ).bindPopup(
+                        '<div> nom : ' + user.username + '</div>' + '<div> mail : ' + user.email + '</div>'
+                    ).addTo(resellers);
+                }
+
+                if (user.collecte_point) {
+                    L.marker(
+                        [Number(user.lat), Number(user.lon)],
+                        {icon: collectePointIcon}
+                    ).bindPopup(
+                        '<div> nom : ' + user.username + '</div>' + '<div> mail : ' + user.email + '</div>'
+                    ).addTo(collecte_points);
+                }
             }
         })
-        // switch case :
-/*
-        users.map(user => {
-            if (user.lat && user.lon) {
-                switch (user.reseller && user.producer && user.collecte_point) {
-                    case true && true && true : // distributeur + producteur + point de collecte
-                        return console.log('all')
-                        const marker = L.marker([Number(user.lat), Number(user.lon)], {icon:producersIcon}).bindPopup('<div> nom : '+user.username +'</div>' + '<div> mail : ' + user.email+'</div>');
-                        marker.addTo(map);;
-                    case true && false && true :// distributeur + point de collecte
-                        return console.log('distrib et ptdC');
-                    case true && false && false : // distributeur
-                        return console.log('distrib');
-                    case false && true && false : // producteur
-                        return console.log('producteur');
-                //const marker = L.marker([Number(user.lat), Number(user.lon)], {icon:producersIcon}).bindPopup('<div> nom : '+user.username +'</div>' + '<div> mail : ' + user.email+'</div>');
-               // marker.addTo(map);
-            }
-        })*/
+        return  {
+            'Producteurs': producers,
+            'Distributeurs': resellers,
+            'Points de collectes': collecte_points
+        };
     }
+
     /*
     makeCapitalCircleMarkers(map: L.Map): void {
         this.http.get(this.acteurs).subscribe((res: any) => {
--- src/app/map/view/view.component.ts
+++ src/app/map/view/view.component.ts
@@ -1,28 +1,10 @@
-import {AfterViewInit, Component, Input, OnInit} from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
 import * as L from 'leaflet';
 import 'leaflet-routing-machine';
-// import 'lrm-mapbox';
 import {MarkerService} from '../services/marker.service';
 import {StatistiqueService} from '../services/statistique.service';
-import {UsersDataSource} from "../../user/data/users-data-source";
 import {UserService} from "../../user/services/user.service";
 import {User} from "../../user/data/User";
-import {NominatimService} from "../../user/services/nominatim.service";
-
-// const iconRetinaUrl = 'assets/marker-icon-2x.png';
-// const iconUrl = 'assets/marker-icon.png';
-// const shadowUrl = 'assets/marker-shadow.png';
-// const iconDefault = L.icon({
-//     iconRetinaUrl,
-//     iconUrl,
-//     shadowUrl,
-//     iconSize: [25, 41],
-//     iconAnchor: [12, 41],
-//     popupAnchor: [1, -34],
-//     tooltipAnchor: [16, -28],
-//     shadowSize: [41, 41]
-// });
-// L.Marker.prototype.options.icon = iconDefault; ok
 
 @Component({
     selector: 'app-view',
@@ -33,6 +15,7 @@ export class ViewComponent implements AfterViewInit {
     users: User[] = [];
     private map!: L.Map;
     private isochrones: any;
+    private baseLayers: any;
 
     constructor(
         private markerService: MarkerService,
@@ -41,11 +24,48 @@ export class ViewComponent implements AfterViewInit {
     ) {
     }
 
+    ngAfterViewInit(): void {
+        this.initMap();
+        this.getUsers();
+        this.statistiqueService.getIsochroneStats().subscribe(AA_MINS => {
+            this.isochrones = AA_MINS;
+            this.initIsochronesLayer();
+        })
+    }
+    private initMap(): void {
+        const mapboxAttribution = 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>';
+        const mapboxUrl = 'https://api.mapbox.com/styles/v1/sangis/cjz8itspx0b771cqfmhddl6sx/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic2FuZ2lzIiwiYSI6Ii1YT3B3encifQ.DuJG8BEzs7jc79vHmB4ytg';
+        const fonds_noir = L.tileLayer(mapboxUrl, {
+            id: 'mapbox/Moonlight',
+            tileSize: 512,
+            zoomOffset: -1,
+            attribution: mapboxAttribution
+        });
+
+        const streets = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 18,
+            minZoom: 3,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+            className: "OSM"
+        });
+
+        this.map = L.map('map', {
+            center: [43.765, 3.641],
+            zoom: 9,
+            layers: [fonds_noir]
+        });
+
+        this.baseLayers = {
+            'Streets': streets,
+            'Moonlight': fonds_noir
+        };
+    }
+
     getUsers() {
         this.userService.getAllPublicUser().subscribe(
             (users) => {
                 this.users = users;
-                this.markerService.makeCapitalMarkers(this.map, this.users);
+                this.addMarkers();
             },
             error => {
                 console.error(error)
@@ -53,48 +73,10 @@ export class ViewComponent implements AfterViewInit {
         )
     }
 
-    ngAfterViewInit(): void {
-        this.initMap();
-        this.getUsers()
-        // this.markerService.makeCapitalCircleMarkers(this.map);
-        this.statistiqueService.getIsochroneStats().subscribe(AA_MINS => {
-            this.isochrones = AA_MINS;
-            this.initIsochronesLayer();
-        })
-    }
-    private initMap(): void {
-        var mapboxAttribution = 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>';
-        var mapboxUrl = 'https://api.mapbox.com/styles/v1/sangis/cjz8itspx0b771cqfmhddl6sx/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic2FuZ2lzIiwiYSI6Ii1YT3B3encifQ.DuJG8BEzs7jc79vHmB4ytg';
-        var fonds_noir = L.tileLayer(mapboxUrl, {id: 'mapbox/Moonlight', tileSize: 512, zoomOffset: -1, attribution: mapboxAttribution});
-     
-        var streets = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 18,
-            minZoom: 3,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-            className: "OSM"
-        });
-
-        var acteurs = L.layerGroup();
-
-        var producers =  L.marker([43.765, 3.641]).bindPopup('Ceci est un brasseur !!!').addTo(acteurs);
-        //var producers =  L.marker([this.user.lon, 3.641]).bindPopup('Ceci est un brasseur !!!').addTo(acteurs);
-
-        this.map = L.map('map', {
-            center: [43.765, 3.641],
-            zoom: 9,
-            layers: [fonds_noir, acteurs]
-        });
-
-        var baseLayers = {
-            'Streets': streets,
-            'Moonlight': fonds_noir
-        };
-       
-        var overlays = {
-            'Acteurs': acteurs
-        };
-
-        var layerControl = L.control.layers(baseLayers, overlays).addTo(this.map);
+    addMarkers() {
+        let overlays = this.markerService.makeCapitalMarkers(this.map, this.users);
+        // console.log(overlays)
+        L.control.layers(this.baseLayers, overlays).addTo(this.map);
     }
 
     private initIsochronesLayer() {
