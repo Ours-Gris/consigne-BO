a611b6ebee2b2e54f3f17908fb5689d1269c77bb
Raphael Vignau <raphael.vignau.dev@gmail.com>
2022-03-11 14:21:26 +0100

    Correction mineur

--- src/app/map/map-routing.module.ts
+++ src/app/map/map-routing.module.ts
@@ -1,11 +1,15 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {MapComponent} from "../pages/map/map.component";
+import {PassageMapComponent} from "./passage-map/passage-map.component";
 
 const routes: Routes = [
     {
         path: '',
         component: MapComponent
+    }, {
+        path: 'passage',
+        component: PassageMapComponent
     },
 ];
 
